900da2222a89bdf1e959b63f9a600cffe8b610c6
rohitprocode <rohit990797@gmail.com>
2024-01-21 19:17:32 +0530

    Netflix Clone

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Container from './components/Container';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Container/>
+    </div>
+  );
+}
+
+export default App;
